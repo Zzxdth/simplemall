6596e86e0e0adb78770996c5099644e72dd0ba00
zhangth1013@163.com <xd5210822>
2020-07-05 23:17:09 +0800

    增加公共管理类  为product 、 order 增加crud代码

--- simplemall-product/src/main/java/com/taihua/simplemall/product/SimplemallProductApplication.java
+++ simplemall-product/src/main/java/com/taihua/simplemall/product/SimplemallProductApplication.java
@@ -1,8 +1,10 @@
 package com.taihua.simplemall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.taihua.simplemall.product.dao")
 @SpringBootApplication
 public class SimplemallProductApplication {
 
